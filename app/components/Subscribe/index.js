63d41d01272556ad9d841ed4e7eda90013044be4
Shakti Kadapure <shakti.kadapure@gmail.com>
2018-02-14 19:17:22 -0500

    getting subscribe to work

--- app/components/Subscribe/index.js
+++ app/components/Subscribe/index.js
@@ -1,16 +1,60 @@
 import React from 'react';
 import Input from 'components/Input';
 import Button from 'components/Button';
+import Validator from 'email-validator';
+import axios from 'axios';
 
 class Subscribe extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.subscribe = this.subscribe.bind(this);
     this.state = {
       emailId: '',
-      subscribe: false,
+      subscribeResult: 'Subscribe',
     };
   }
 
+  handleChange(event) {
+    const value = event.target.value;
+    this.setState({
+      emailId: value,
+    });
+    // if(Validator.validate(value)) {}
+  }
+
+  subscribe() {
+    const emailid = this.state.emailId;
+    if (Validator.validate(emailid)) {
+      this.postEmailId(emailid, this);
+    }
+  }
+
+  postEmailId(emailid, scope) {
+    return axios({
+      url: 'https://6tdxaipp5a.execute-api.us-east-1.amazonaws.com/prod/dummy',
+      timeout: 20000,
+      method: 'post',
+      data: {
+        emailid,
+      },
+      headers: {
+        accept: 'application/json',
+        'accept-language': 'en_US',
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+    })
+    .then(() => {
+      scope.setState({
+        subscribeResult: 'SUCCESS',
+      });
+    })
+    .catch(() => {
+      scope.setState({
+        subscribeResult: 'FAILURE',
+      });
+    });
+  }
   render() {
     return (
       <div style={{ marginBottom: '1rem' }}>
@@ -18,9 +62,13 @@ class Subscribe extends React.Component {
           id="username"
           type="text"
           placeholder="email id"
+          onChange={this.handleChange}
+          style={{ minWidth: '220px' }}
         />
-        <Button>
-          Subscribe
+        <Button
+          onClick={this.subscribe}
+        >
+          {this.state.subscribeResult}
         </Button>
       </div>
     );
