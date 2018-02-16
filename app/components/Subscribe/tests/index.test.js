aa28b305392a3428b5b436f041af808751bc3995
Shakti Kadapure <shakti.kadapure@gmail.com>
2018-02-15 19:41:21 -0500

    - temporarily disabling Subscribe component test coverage  - fixing tests on subscribe component

--- app/components/Subscribe/tests/index.test.js
+++ app/components/Subscribe/tests/index.test.js
@@ -5,13 +5,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Subscribe from 'components/Subscribe';
-import Form from 'components/Form';
+import Button from 'components/Button';
 import Input from 'components/Input';
 
 describe('Subscribe email test', () => {
   it('should render the subscribe component', () => {
     const renderedComponent = shallow(<Subscribe />);
-    expect(renderedComponent.find(Form).length).toBe(1);
+    expect(renderedComponent.find(Button).length).toBe(1);
     expect(renderedComponent.find(Input).length).toBe(1);
   });
 });
