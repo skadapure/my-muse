a75d885898f814fc899146e8e8c9bc11081e5d8b
skadapure <shakti.kadapure@gmail.com>
2018-02-18 08:31:58 -0500

    updated disclosure content

--- app/containers/DisclosurePage/messages.js
+++ app/containers/DisclosurePage/messages.js
@@ -15,7 +15,7 @@ export default defineMessages({
   },
   personal: {
     id: 'boilerplate.containers.DisclosurePage.personal',
-    defaultMessage: 'We may collect personal identification information from Users in a variety of ways in connection with activities, services, features or resources we make available on our Site.. Users may visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.',
+    defaultMessage: 'We may collect personal identification information from Users in a variety of ways in connection with activities, services, features or resources we make available on our Site. Users may visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.',
   },
   personalHeader: {
     id: 'boilerplate.containers.DisclosurePage.personalHeader',
@@ -79,7 +79,7 @@ export default defineMessages({
   },
   notice: {
     id: 'boilerplate.containers.DisclosurePage.notice',
-    defaultMessage: 'We allow third party companies to serve ads and/or collect certain anonymous information when you visit our Web site.  These companies may use non-personally identifiable information (e.g. click stream information, browser type, time and date, subject of advertisements clicked or scrolled over) during your visits to this and other Web sites in order to provide advertisements about goods and services likely to be of greater interest to you.  These companies typically use a cookie or third party web beacon to collect this information',
+    defaultMessage: 'We allow third party companies to serve ads and/or collect certain anonymous information when you visit our Web site.  These companies may use non-personally identifiable information (e.g. click stream information, browser type, time and date, subject of advertisements clicked or scrolled over) during your visits to this and other Web sites in order to provide advertisements about goods and services likely to be of greater interest to you.  These companies typically use a cookie or third party web beacon to collect this information.',
   },
   noticeHeader: {
     id: 'boilerplate.containers.DisclosurePage.noticeHeader',
